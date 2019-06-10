31f72257816d4491a9048277414b08df32592f7b
hzqst <113660872@qq.com>
2019-06-10 11:17:04 +0800

    1

---
+++ 队友脚本.js
@@ -0,0 +1,117 @@
+//begin from anywhere
+
+var cga = require('./cgaapi')(function(){
+	console.log('队员脚本，起始地点任意，回补提醒+卖石，血或者蓝量少于xx%立刻提醒队长')
+	
+	var minHp = 0.3;//50%hp提醒
+	var minPetHp = 0.3;//宠30%hp提醒
+	var minMp = 0.2;//10%mp提醒
+
+	var mute = 0;
+	
+	var playerinfo = cga.GetPlayerInfo();
+	
+	var teammates = [];
+	
+	var teamplayers = cga.getTeamPlayers();
+
+	for(var i in teamplayers)
+		teammates[i] = teamplayers[i].name;
+	
+	cga.isTeamLeader = (teammates[0] == playerinfo.name) ? true : false;
+	
+	var waitLeader = ()=>{
+		
+	}
+	
+	var waitArray = [
+	{
+		mapname:'工房',
+		pos:[21, 23],
+		cb : ()=>{
+			cga.TurnTo(21, 23);
+			cga.sellStone(function(){
+				cga.SayWords('卖石完毕...', 0, 3, 1);
+				setTimeout(cga.waitForMultipleLocation, 15000, waitArray);
+			});
+		}
+	},
+	{
+		mapname:'里谢里雅堡',
+		pos:[30, 77],
+		cb : ()=>{
+			cga.TurnTo(30, 77);
+			cga.sellStone(function(){
+				cga.SayWords('卖石完毕...', 0, 3, 1);
+				setTimeout(cga.waitForMultipleLocation, 15000, waitArray);
+			});
+		}
+	},
+	{
+		mapindex:27101,//辛希亚探索指挥部
+		pos:[7, 21],
+		cb : ()=>{
+			
+			if(!cga.isTeamLeader && teammates.length > 0){
+				
+				var waitAdd = ()=>{
+					
+					cga.addTeammate(teammates[0], (r)=>{
+						if(r){
+							setTimeout(cga.waitForMultipleLocation, 1500, waitArray);
+							return;
+						}
+						setTimeout(waitAdd, 1000);
+					});
+				}
+				var retry = ()=>{
+					cga.TurnTo(7, 21);
+					cga.AsyncWaitMovement({map:'圣骑士营地', delay:1000, timeout:5000}, (r)=>{
+						if(r == true){
+							waitAdd();
+							return;
+						}
+						
+						cga.walkList([ [8, 21], [8, 22] ], retry);
+					});
+				}
+				
+				retry();
+			} else {
+				setTimeout(cga.waitForMultipleLocation, 1500, waitArray);
+			}
+		}
+	}
+	];
+
+	cga.waitForMultipleLocation(waitArray);
+	
+	setInterval(()=>{
+
+		if(cga.isInBattle())
+			return;
+
+		if(new Date().getTime() - mute >= 1000 * 15)//15s内闭嘴
+		{
+			var playerinfo = cga.GetPlayerInfo();
+			var petinfo = cga.GetPetInfo(playerinfo.petid);
+			
+			if(playerinfo.health > 50){
+				cga.LogBack();
+				process.exit(1);
+				return;
+			}
+			
+			if(playerinfo.mp < playerinfo.maxmp * minMp)
+				cga.SayWords('人物蓝量不够，需要回补!', 0, 3, 1);
+			else if(playerinfo.hp < playerinfo.maxhp * minHp)
+				cga.SayWords('人物血量不够，需要回补!', 0, 3, 1);
+			else if(petinfo.mp < petinfo.maxmp * minMp)
+				cga.SayWords('宠物蓝量不够，需要回补!', 0, 3, 1);
+			else if(petinfo.hp < petinfo.maxhp * minPetHp)
+				cga.SayWords('宠物血量不够，需要回补!', 0, 3, 1);
+			
+			mute = new Date().getTime();
+		}
+	}, 1000);
+});
