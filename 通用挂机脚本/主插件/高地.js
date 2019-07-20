2d7ec3234d8d57dc1ef8fa511598292e3047fdc9
hzqst <113660872@qq.com>
2019-07-20 18:29:49 +0800

    remove unnecessary scripts

---
+++ 通用挂机脚本/主插件/高地.js
@@ -0,0 +1,203 @@
+var Async = require('async');
+var supplyMode = require('./../公共模块/高地回补');
+var teamMode = require('./../公共模块/组队模式');
+
+var battleAreaArray = [
+{
+	name : '刀鸡',
+	pos : [34, 188],
+	dir : 0,
+},
+{
+	name : '黄金龙骨',
+	pos : [135, 175],
+	dir : 6,
+},
+{
+	name : '银狮',
+	pos : [147, 117],
+	dir : 2,
+},
+]
+
+var cga = global.cga;
+var configTable = global.configTable;
+
+var battle = ()=>{
+	var playerinfo = cga.GetPlayerInfo();
+	var ctx = {
+		playerinfo : playerinfo,
+		petinfo : cga.GetPetInfo(playerinfo.petid),
+		teamplayers : cga.getTeamPlayers(),
+		result : null,
+	}
+
+	teamMode.battle(ctx);
+
+	global.callSubPlugins('battle', ctx);
+
+	if( ctx.result == 'supply' ){
+
+		console.log(ctx.reason);
+
+		supplyMode.func(loop);
+		
+		return false;
+	}
+	else if( ctx.result == 'logback' ){
+		cga.LogBack();
+		setTimeout(loop, 1500);
+		return false;
+	}
+	
+	return true;
+}
+
+var loop = ()=>{
+	var map = cga.GetMapName();
+	var mapindex = cga.GetMapIndex().index3;
+	
+	if(cga.isTeamLeader == true || !cga.getTeamPlayers().length){
+		if(map == '医院' && mapindex == 59539){
+			cga.walkList([
+			[28, 52, '艾夏岛'],
+			[190, 116, '盖雷布伦森林'],
+			[231, 222, '布拉基姆高地'],
+			thisobj.battleArea.pos,
+			], loop);
+			return;
+		} 
+		if(map == '布拉基姆高地')
+		{
+			cga.freqMove(thisobj.battleArea.dir, ()=>{
+				
+				if(!cga.isInBattle()) {
+					if(!battle())
+						return false;
+				}
+				
+				return true;
+			});
+			return;
+		}
+		if(map == '艾尔莎岛' && teamMode.is_enough_teammates()){
+			cga.travel.newisland.toStone('D', ()=>{
+				cga.walkList([
+					[190, 116, '盖雷布伦森林'],
+					[231, 222, '布拉基姆高地'],
+					thisobj.battleArea.pos,
+				], loop);
+			});
+			return;
+		}
+	} else {
+		if(!cga.isInBattle()) {
+			if(!battle())
+				return;
+		}
+		
+		setTimeout(loop, 1500);
+		return;
+	}
+
+	if(cga.needSupplyInitial())
+	{
+		cga.travel.falan.toCastleHospital(()=>{
+			setTimeout(loop, 5000);
+		});
+		return;
+	}
+	
+	cga.travel.newisland.toStone('X', ()=>{
+		cga.walkList([
+		cga.isTeamLeader ? [144, 106] : [143, 106],
+		], ()=>{
+			teamMode.wait_for_teammates(loop);
+		});
+	});
+}
+
+var thisobj = {
+	getDangerLevel : ()=>{
+		var map = cga.GetMapName();
+
+		if(map == '盖雷布伦森林' )
+			return 1;
+
+		if(map == '布拉基姆高地' )
+			return 2;
+		
+		return 0;
+	},
+	translate : (pair)=>{
+		
+		if(pair.field == 'battleArea'){
+			pair.field = '练级地点';
+			pair.value = battleAreaArray[pair.value].name;
+			pair.translated = true;
+			return true;
+		}
+		
+		if(supplyMode.translate(pair))
+			return true;
+		
+		if(teamMode.translate(pair))
+			return true;
+		
+		return false;
+	},
+	loadconfig : (obj)=>{
+
+		if(!supplyMode.loadconfig(obj))
+			return false;
+
+		if(!teamMode.loadconfig(obj))
+			return false;
+		
+		for(var i in battleAreaArray){
+			if(i == obj.battleArea){
+				configTable.battleArea = i;
+				thisobj.battleArea = battleAreaArray[i];
+				break;
+			}
+		}
+		
+		if(!thisobj.battleArea){
+			console.error('读取配置：练级地点失败！');
+			return false;
+		}
+		
+		return true;
+	},
+	inputcb : (cb)=>{
+		Async.series([supplyMode.inputcb, teamMode.inputcb, (cb2)=>{
+			
+			var sayString = '【高地插件】请选择练级地点:';
+			for(var i in battleAreaArray){
+				if(i != 0)
+					sayString += ', ';
+				sayString += '('+ (parseInt(i)+1) + ')' + battleAreaArray[i].name;
+			}
+			cga.sayLongWords(sayString, 0, 3, 1);
+			cga.waitForChatInput((msg)=>{
+				var index = parseInt(msg);
+				if(index >= 1 && battleAreaArray[index - 1]){
+					configTable.battleArea = index - 1;
+					thisobj.battleArea = battleAreaArray[index - 1];
+					
+					var sayString2 = '当前已选择:[' + thisobj.battleArea.name + ']。';
+					cga.sayLongWords(sayString2, 0, 3, 1);
+					
+					cb2(null);
+					
+					return true;
+				}
+				
+				return false;
+			});
+		}], cb);
+	},
+	execute : loop,
+}
+
+module.exports = thisobj;
---
+++ 通用挂机脚本/公共模块/高地回补.js
@@ -0,0 +1,107 @@
+var supplyModeArray = [
+{
+	name : '艾夏岛医院,资深护士回补',
+	func : (cb)=>{
+		cga.walkList([
+			[30, 193, '盖雷布伦森林'],
+			[199, 211, '艾夏岛'],
+			[112, 81, '医院'],
+			[35, 43],
+			[35, 42],
+			[35, 43],
+			[35, 42],
+			[35, 43],
+		], ()=>{
+			cga.TurnTo(37, 43);
+			setTimeout(cb, 5000);
+		});
+	}
+},
+{
+	name : '艾夏岛医院,普通护士回补',
+	func : (cb)=>{
+		cga.walkList([
+			[30, 193, '盖雷布伦森林'],
+			[199, 211, '艾夏岛'],
+			[112, 81, '医院'],
+			[35, 46],
+			[35, 45],
+			[35, 46],
+			[35, 45],
+			[35, 46],
+		], ()=>{
+			cga.TurnTo(37, 46);
+			setTimeout(cb, 5000);
+		});
+	}
+},
+{
+	name : '登出飞碟回补',
+	func : (cb)=>{
+		cga.travel.falan.toCastleHospital(()=>{
+			setTimeout(cb, 5000);
+		});
+	}
+}
+]
+
+var cga = global.cga;
+var configTable = global.configTable;
+
+var thisobj = {
+	func : (cb)=>{
+		thisobj.object.func(cb);
+	},
+	translate : (pair)=>{
+		if(pair.field == 'supplyMode'){
+			pair.field = '回补方式';
+			pair.value = supplyModeArray[pair.value].name;
+			pair.translated = true;
+			return true;
+		}
+		return false;
+	},
+	loadconfig : (obj)=>{
+		for(var i in supplyModeArray){
+			if(i == obj.supplyMode){
+				configTable.supplyMode = i;
+				thisobj.object = supplyModeArray[i];
+				break;
+			}
+		}
+		
+		if(!thisobj.object){
+			console.error('读取配置：回补方式失败！');
+			return false;			
+		}
+		
+		return true;
+	},
+	inputcb : (cb)=>{
+		var sayString = '【高地插件】请选择回补方式:';
+		for(var i in supplyModeArray){
+			if(i != 0)
+				sayString += ', ';
+			sayString += '('+ (parseInt(i)+1) + ')' + supplyModeArray[i].name;
+		}
+		cga.sayLongWords(sayString, 0, 3, 1);
+		cga.waitForChatInput((msg)=>{
+			var index = parseInt(msg);
+			if(index >= 1 && supplyModeArray[index - 1]){
+				configTable.supplyMode = index - 1;
+				thisobj.object = supplyModeArray[index - 1];
+				
+				var sayString2 = '当前已选择:[' + thisobj.object.name + ']。';
+				cga.sayLongWords(sayString2, 0, 3, 1);
+				
+				cb(null);
+				
+				return true;
+			}
+			
+			return false;
+		});
+	}
+}
+
+module.exports = thisobj;
