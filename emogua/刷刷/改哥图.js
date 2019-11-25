c7f7b82ea5970679fe17e388ee0124b36e226182
emogua <emogua@163.com>
2019-11-25 19:50:20 +0800

    add script

---
+++ emogua/刷刷/改哥图.js
@@ -0,0 +1,133 @@
+/**
+ * 默认单刷
+ * 需要开亚留特传送
+ * 每一个列表为一队，每个列表的第一个名字是队长,如果角色找不到队伍视为单刷
+ */
+const teams = [];
+require('../wrapper').then(cga => {
+	const player = cga.GetPlayerInfo();
+	let team = teams.find(t => t.find(n => n == player.name));
+	if (!team) {
+		team = [player.name];
+	}
+	console.log('team', team);
+	const captain = team[0];
+	const isCaptain = player.name == captain;
+
+	let npcPosition;
+	let firtIndex;
+	cga.emogua.autoBattle(cga.emogua.AutoBattlePreset.getEscapeSets());
+	cga.emogua.prepare().then(
+		() => cga.emogua.recursion(
+			() => Promise.resolve().then(() => {
+				if (cga.GetPlayerInfo().gold < 4000) {
+					console.log('没有足够的金币');
+					return Promise.reject();
+				}
+				if (cga.getInventoryItems().length == 20) {
+					console.log('背包满了');
+					return Promise.reject();
+				}
+				if (cga.emogua.getTeamNumber() == 1 && cga.GetMapName() != '亚留特村') {
+					return cga.emogua.goto(n => n.teleport.aleut);
+				}
+			}).then(() => {
+				if (isCaptain) {
+					return cga.emogua.autoWalk([52,47]).then(
+						() => cga.emogua.waitTeamBlock(team.length)
+					).then(
+						() => cga.emogua.autoWalkList([
+							[52,63,'*'],[10,5]
+						])
+					).then(
+						() => cga.emogua.recharge(0)
+					).then(
+						() => cga.emogua.autoWalkList([
+							[2,9,'*'],[58,31,'*'],[551,26]
+						])
+					).then(
+						() => cga.emogua.recursion(() => {
+							const entry = cga.getMapObjects().find(m => m.cell == 3 && m.x > 529 && m.x < 549 && m.y > 26 && m.y < 48);
+							if (entry) {
+								const around = cga.emogua.getMovablePositionAround(entry);
+								return cga.emogua.autoWalk([around.x, around.y], undefined, 0, false).then(
+									() => cga.emogua.delay(5000)
+								).then(
+									() => cga.emogua.autoWalk([entry.x, entry.y, '*'])
+								).then(() => {
+									const mapIndexes = cga.GetMapIndex();
+									if (firtIndex != mapIndexes.index3) {
+										npcPosition = undefined;
+										firtIndex = mapIndexes.index3;
+									}
+									return Promise.reject()
+								});
+							}
+							return cga.emogua.delay(10000);
+						})
+					).then(() => {
+						if (npcPosition) {
+							return cga.emogua.walkRandomMazeUntil(() => cga.GetMapName() == npcPosition.mapName).then(() => {
+								const up = cga.emogua.getFarthestEntry(npcPosition.start);
+								const walkTo = cga.emogua.getMovablePositionAround({x: npcPosition.xpos, y: npcPosition.ypos})
+								return cga.emogua.autoWalk([walkTo.x, walkTo.y],undefined,0,false).then(
+									() => cga.emogua.talkNpc(npcPosition.xpos, npcPosition.ypos, cga.emogua.talkNpcSelectorYes)
+								).then(
+									() => cga.emogua.autoWalk([up.x, up.y, '*'],undefined,0,false)
+								);
+							});
+						}
+						return cga.emogua.searchMap(
+							units => units.find(u => u.unit_name == '无照护士米内鲁帕' && u.type == 1)
+						).then(unit => {
+							if (typeof unit == 'object') {
+								npcPosition = unit;
+								npcPosition.mapName = cga.GetMapName();
+								const up = cga.emogua.getFarthestEntry(npcPosition.start);
+								return cga.emogua.talkNpc(npcPosition.xpos, npcPosition.ypos, cga.emogua.talkNpcSelectorYes).then(
+									() => cga.emogua.autoWalk([up.x, up.y, '*'],undefined,0,false)
+								);
+							}
+						});
+					}).then(() => {
+						if (cga.GetMapName() != '阿鲁巴斯实验所') {
+							return cga.emogua.walkRandomMazeUntil(() => cga.GetMapName() == '阿鲁巴斯实验所');
+						}
+					}).then(
+						() => cga.emogua.autoWalk([21,19])
+					).then(
+						() => cga.emogua.autoBattle(cga.emogua.AutoBattlePreset.getAttackSets())
+					).then(
+						() => cga.emogua.talkNpc(21, 18, cga.emogua.talkNpcSelectorYes)
+					).then(
+						() => cga.emogua.delay(2000)
+					).then(
+						() => cga.emogua.waitAfterBattle()
+					).then(() => {
+						cga.emogua.autoBattle(cga.emogua.AutoBattlePreset.getEscapeSets());
+						if (cga.GetMapName() == '阿鲁巴斯研究所') {
+							return cga.emogua.autoWalk([12,15]).then(
+								() => cga.emogua.turnOrientation(6,'*')
+							).then(
+								() => cga.emogua.autoWalk([9,7])
+							).then(
+								() => cga.emogua.talkNpc(10, 7, cga.emogua.talkNpcSelectorYes)
+							);
+						}
+					}).then(
+						() => cga.emogua.autoWalk([588,51,'*'])
+					);
+				}
+				if (cga.emogua.getTeamNumber() == 1) return cga.emogua.autoWalk([52,48]).then(
+					() => cga.emogua.joinTeamBlock(52, 47, captain)
+				);
+				return cga.emogua.delay(5000);
+			}).catch(r => {
+				console.log('随即迷宫可能刷新,回亚留特重新开始');
+				if (cga.GetMapName() == '芙蕾雅') {
+					return cga.emogua.autoWalk([588,51,'*']);
+				}
+			})
+		)
+	);
+});
---
+++ emogua/练级/罪1.js
@@ -0,0 +1,80 @@
+const teams = [
+	['队长', '队员2', '队员3', '队员4', '队员5']
+];
+const protect = {
+	minHp: 300,
+	minMp: 30,
+	minPetHp: 300,
+	minPetMp: 0,
+	maxItemNumber: 19
+};
+const prepareOptions = {
+	crystalName: '火风的水晶（5：5）'
+};
+
+require('../wrapper').then(cga => {
+	console.log('罪一练级(90怪), 需要开启者称号(UD)');
+	const {team, captain, isCaptain, teamNumber} = cga.emogua.parseTeams(teams);
+	protect.minTeamNumber = teamNumber;
+
+	cga.emogua.autoBattle(cga.emogua.AutoBattlePreset.getAttackSets());
+	cga.emogua.recursion(() => Promise.resolve().then(() => {
+		const currentMap = cga.GetMapName();
+		const currentTeamNumber = cga.emogua.getTeamNumber();
+
+		if (cga.getItemCount('灵魂晶石') == 0) {
+			return cga.emogua.goto(n => n.castle.x).then(
+				() => cga.emogua.autoWalk([34,71])
+			).then(
+				() => cga.emogua.talkNpc(34,70,cga.emogua.talkNpcSelectorYes)
+			);
+		}
+		if (currentMap == '杰诺瓦镇') {
+			if (currentTeamNumber == 1) {
+				if (isCaptain) {
+					return cga.emogua.autoWalk([56,43]).then(
+						() => cga.emogua.waitTeamBlock(teamNumber, team)
+					).then(
+						() => cga.emogua.autoWalkList([
+							[24,40,'莎莲娜'],[150,335],[151,334],[150,335],[151,334],[150,335]
+						])
+					).then(
+						() => cga.emogua.sayWords('对话神官')
+					).then(
+						() => cga.emogua.talkNpc(150,334,cga.emogua.talkNpcSelectorNo,'*')
+					);
+				}
+				return cga.emogua.autoWalk([57,43]).then(
+					() => cga.emogua.joinTeamBlock(56,43,captain)
+				).then(
+					() => cga.emogua.waitMessageUntil(chat => chat.msg && chat.msg.indexOf('对话神官') > -1)
+				).then(
+					() => cga.emogua.talkNpc(150,334,cga.emogua.talkNpcSelectorYes,'*')
+				);
+			}
+		}
+		if (currentMap == '贝兹雷姆小道') {
+			if (currentTeamNumber == 1) {
+				if (isCaptain) {
+					return cga.emogua.autoWalk([32,53]).then(
+						() => cga.emogua.waitTeamBlock(teamNumber)
+					).then(
+						() => cga.emogua.encounter(protect)
+					);
+				}
+				return cga.emogua.joinTeamBlock(32,53,captain);
+			}
+		}
+		if (currentTeamNumber == 1) {
+			return cga.emogua.logBack().then(
+				() => cga.emogua.prepare(prepareOptions)
+			).then(
+				() => cga.emogua.goto(n => n.teleport.jenova)
+			);
+		}
+		if (!isCaptain) {
+			cga.emogua.checkStopEncounter(protect, true);
+		}
+		return cga.emogua.delay(5000);
+	}).catch(console.log));
+});
