d75e55f438de58b2217eb5e05fe5f2a20aef3e18
ltfjasper <237783510@qq.com>
2022-03-24 15:36:10 +0800

    修复承认班车脚本

--- leo/任务--承认之戒(班车).js
+++ leo/任务--承认之戒(班车).js
@@ -11,7 +11,7 @@ require(process.env.CGA_DIR_PATH_UTF8+'/leo').then(async (红叶の脚本) => {
 	启动先登出 = false;
 	
     leo['\x6c\x6f\x67'](班车旗号 + '\u306e\u627f\u8ba4\u4e4b\u6212\u516c\u76ca\u73ed\u8f66\uff0c\u542f\u52a8\x7e'), 红叶の脚本['\x45\x6e\x61\x62\x6c\x65\x46\x6c\x61\x67\x73'](红叶の脚本['\x45\x4e\x41\x42\x4c\x45\x5f\x46\x4c\x41\x47\x5f\x54\x45\x41\x4d\x43\x48\x41\x54'], ![]), 红叶の脚本['\x45\x6e\x61\x62\x6c\x65\x46\x6c\x61\x67\x73'](红叶の脚本['\x45\x4e\x41\x42\x4c\x45\x5f\x46\x4c\x41\x47\x5f\x4a\x4f\x49\x4e\x54\x45\x41\x4d'], !![]), 红叶の脚本['\x45\x6e\x61\x62\x6c\x65\x46\x6c\x61\x67\x73'](红叶の脚本['\x45\x4e\x41\x42\x4c\x45\x5f\x46\x4c\x41\x47\x5f\x43\x41\x52\x44'], ![]), 红叶の脚本['\x45\x6e\x61\x62\x6c\x65\x46\x6c\x61\x67\x73'](红叶の脚本['\x45\x4e\x41\x42\x4c\x45\x5f\x46\x4c\x41\x47\x5f\x54\x52\x41\x44\x45'], ![]);
-    var _0x342670 = '\u6c34\u706b\u7684\u6c34\u6676\uff08\x35\uff1a\x35\uff09', _0x6e3dec = '\u533b\u9053\u4e4b\u6b87', _0x316005 = !![], _0xcadc27 = [], _0x57acc5 = !![], _0x945fc2 = !![], _0x878356 = [
+    var _0x319600 = '\u6c34\u706b\u7684\u6c34\u6676\uff08\x35\uff1a\x35\uff09', _0x186d5d = '\u533b\u9053\u4e4b\u6b87', _0x1cc318 = 红叶の脚本['\x47\x65\x74\x50\x6c\x61\x79\x65\x72\x49\x6e\x66\x6f'](), _0x1b88cc = _0x1cc318['\x6e\x61\x6d\x65'], _0x2eba51 = !![], _0x286023 = [], _0x4d3962 = !![], _0x4b2c63 = !![], _0x4362d4 = [
             [
                 0x27,
                 0x54
@@ -34,52 +34,52 @@ require(process.env.CGA_DIR_PATH_UTF8+'/leo').then(async (红叶の脚本) => {
             ]
         ];
     console['\x6c\x6f\x67'](leo['\x6c\x6f\x67\x54\x69\x6d\x65']() + '\u51fa\u6218\u5ba0\u7269\u662f\uff1a\u3010' + 出战宠名称 + '\u3011');
-    var _0x5c3c7e = 红叶の脚本['\x47\x65\x74\x50\x65\x74\x73\x49\x6e\x66\x6f']()['\x66\x69\x6e\x64'](_0x43249d => _0x43249d['\x72\x65\x61\x6c\x6e\x61\x6d\x65'] == 出战宠名称);
-    if (!_0x5c3c7e)
+    var _0x44305f = 红叶の脚本['\x47\x65\x74\x50\x65\x74\x73\x49\x6e\x66\x6f']()['\x66\x69\x6e\x64'](_0x2baaa4 => _0x2baaa4['\x72\x65\x61\x6c\x6e\x61\x6d\x65'] == 出战宠名称);
+    if (!_0x44305f)
         return await leo['\x6c\x6f\x67']('\u8eab\u4e0a\u6ca1\u6709\u627e\u5230\u5e94\u8be5\u51fa\u6218\u7684\u5ba0\u7269\u3010' + 出战宠名称 + '\u3011\uff0c\u8bf7\u4fee\u6539\u914d\u7f6e\u540e\u91cd\u65b0\u542f\u52a8\u811a\u672c'), await leo['\x64\x65\x6c\x61\x79'](0x3e8 * 0x3c * 0x3c * 0x2), leo['\x65\x78\x69\x74']();
     启动先登出 && await leo['\x6c\x6f\x67\x42\x61\x63\x6b']();
-    if (_0x316005 && leo['\x68\x61\x73']('\u627f\u8ba4\u4e4b\u6212')) {
-        if (_0x57acc5)
+    if (_0x2eba51 && leo['\x68\x61\x73']('\u627f\u8ba4\u4e4b\u6212')) {
+        if (_0x4d3962)
             await leo['\x64\x72\x6f\x70\x49\x74\x65\x6d\x45\x78']('\u627f\u8ba4\u4e4b\u6212');
         else
             return leo['\x6c\x6f\x67']('\u961f\u957f\u8eab\u4e0a\u5df2\u7ecf\u6709\u3010\u627f\u8ba4\u4e4b\u6212\u3011\uff0c\u8bf7\u5148\u624b\u52a8\u4e22\u5f03\uff0c\u518d\u91cd\u65b0\u542f\u52a8\u811a\u672c');
     }
-    var _0x30128d = 0x4, _0x37cb82 = 0x1e, _0x59bfc7 = (_0x52ca70, _0x565641) => {
-            leo['\x73\x61\x79'](班车旗号 + '\u306e\u627f\u8ba4\u4e4b\u6212\u516c\u76ca\u73ed\u8f66\uff0c\u6b22\u8fce\u9700\u8981\u53bb\u8425\u5730\u7684\u73a9\u5bb6\u4e58\u5750\x7e', _0x30128d), 红叶の脚本['\x45\x6e\x61\x62\x6c\x65\x46\x6c\x61\x67\x73'](红叶の脚本['\x45\x4e\x41\x42\x4c\x45\x5f\x46\x4c\x41\x47\x5f\x4a\x4f\x49\x4e\x54\x45\x41\x4d'], !![]);
-            if (_0x52ca70 <= 0x1)
+    var _0x3ac361 = 0x4, _0x30aaae = 0x1e, _0x10132b = (_0x4b886b, _0x535802) => {
+            leo['\x73\x61\x79'](班车旗号 + '\u306e\u627f\u8ba4\u4e4b\u6212\u516c\u76ca\u73ed\u8f66\uff0c\u6b22\u8fce\u9700\u8981\u53bb\u8425\u5730\u7684\u73a9\u5bb6\u4e58\u5750\x7e', _0x3ac361), 红叶の脚本['\x45\x6e\x61\x62\x6c\x65\x46\x6c\x61\x67\x73'](红叶の脚本['\x45\x4e\x41\x42\x4c\x45\x5f\x46\x4c\x41\x47\x5f\x4a\x4f\x49\x4e\x54\x45\x41\x4d'], !![]);
+            if (_0x4b886b <= 0x1)
                 return leo['\x6e\x65\x78\x74']();
             return leo['\x74\x6f\x64\x6f']()['\x74\x68\x65\x6e'](() => {
-                var _0x5f5c15 = 红叶の脚本['\x67\x65\x74\x54\x65\x61\x6d\x50\x6c\x61\x79\x65\x72\x73']()['\x66\x69\x6c\x74\x65\x72'](_0x34c9d0 => _0x34c9d0['\x6e\x61\x6d\x65'] != playerName && _0x34c9d0['\x6c\x65\x76\x65\x6c'] >= 等级限制);
-                if (_0x5f5c15['\x6c\x65\x6e\x67\x74\x68'] > 0x0)
-                    return leo['\x6b\x69\x63\x6b\x6f\x66\x66'](_0x5f5c15[0x0]['\x6e\x61\x6d\x65'])['\x74\x68\x65\x6e'](() => _0x59bfc7(_0x52ca70, _0x565641));
+                var _0x499c92 = 红叶の脚本['\x67\x65\x74\x54\x65\x61\x6d\x50\x6c\x61\x79\x65\x72\x73']()['\x66\x69\x6c\x74\x65\x72'](_0x568af3 => _0x568af3['\x6e\x61\x6d\x65'] != _0x1b88cc && _0x568af3['\x6c\x65\x76\x65\x6c'] >= 等级限制);
+                if (_0x499c92['\x6c\x65\x6e\x67\x74\x68'] > 0x0)
+                    return leo['\x6b\x69\x63\x6b\x6f\x66\x66'](_0x499c92[0x0]['\x6e\x61\x6d\x65'])['\x74\x68\x65\x6e'](() => _0x10132b(_0x4b886b, _0x535802));
             })['\x74\x68\x65\x6e'](() => {
-                var _0x346368 = 红叶の脚本['\x67\x65\x74\x54\x65\x61\x6d\x50\x6c\x61\x79\x65\x72\x73']();
-                if (_0x346368['\x6c\x65\x6e\x67\x74\x68'] == _0x52ca70) {
-                    leo['\x73\x61\x79'](班车旗号 + '\u306e\u627f\u8ba4\u4e4b\u6212\u516c\u76ca\u73ed\u8f66\uff0c\u5df2\u6ee1\u5458\uff0c\u51fa\u53d1\x7e', _0x30128d), _0xcadc27 = [];
-                    var _0x346368 = 红叶の脚本['\x67\x65\x74\x54\x65\x61\x6d\x50\x6c\x61\x79\x65\x72\x73']();
-                    for (var _0x557b0e in _0x346368) {
-                        _0xcadc27[_0x557b0e] = _0x346368[_0x557b0e]['\x6e\x61\x6d\x65'];
+                var _0x709bda = 红叶の脚本['\x67\x65\x74\x54\x65\x61\x6d\x50\x6c\x61\x79\x65\x72\x73']();
+                if (_0x709bda['\x6c\x65\x6e\x67\x74\x68'] == _0x4b886b) {
+                    leo['\x73\x61\x79'](班车旗号 + '\u306e\u627f\u8ba4\u4e4b\u6212\u516c\u76ca\u73ed\u8f66\uff0c\u5df2\u6ee1\u5458\uff0c\u51fa\u53d1\x7e', _0x3ac361), _0x286023 = [];
+                    var _0x709bda = 红叶の脚本['\x67\x65\x74\x54\x65\x61\x6d\x50\x6c\x61\x79\x65\x72\x73']();
+                    for (var _0x48c602 in _0x709bda) {
+                        _0x286023[_0x48c602] = _0x709bda[_0x48c602]['\x6e\x61\x6d\x65'];
                     }
-                    return leo['\x73\x61\x79']('\u6b22\u8fce\u4e58\u5750' + 班车旗号 + '\u306e\u73ed\u8f66\uff0c\u672c\u6b21\u65c5\u9014\u76ee\u7684\u5730\u662f\x5b\u627f\u8ba4\u4e4b\u6212\x5d\uff0c\u961f\u5458\x5b' + _0xcadc27['\x74\x6f\x53\x74\x72\x69\x6e\x67']() + '\x5d', _0x30128d), leo['\x64\x6f\x6e\x65']();
+                    return leo['\x73\x61\x79']('\u6b22\u8fce\u4e58\u5750' + 班车旗号 + '\u306e\u73ed\u8f66\uff0c\u672c\u6b21\u65c5\u9014\u76ee\u7684\u5730\u662f\x5b\u627f\u8ba4\u4e4b\u6212\x5d\uff0c\u961f\u5458\x5b' + _0x286023['\x74\x6f\x53\x74\x72\x69\x6e\x67']() + '\x5d', _0x3ac361), leo['\x64\x6f\x6e\x65']();
                 } else {
-                    if (_0x346368['\x6c\x65\x6e\x67\x74\x68'] >= _0x565641) {
-                        if (_0x37cb82 > 0x0)
-                            return leo['\x73\x61\x79']('\u5df2\u6ee1\u6700\u5c0f\u4eba\u6570\u3010' + _0x565641 + '\u3011\uff0c\u51fa\u53d1\u5012\u8ba1\u65f6' + _0x37cb82 + '\u79d2', _0x30128d), _0x37cb82 = _0x37cb82 - 0x5, leo['\x64\x65\x6c\x61\x79'](0x1388)['\x74\x68\x65\x6e'](() => _0x59bfc7(_0x52ca70, _0x565641));
+                    if (_0x709bda['\x6c\x65\x6e\x67\x74\x68'] >= _0x535802) {
+                        if (_0x30aaae > 0x0)
+                            return leo['\x73\x61\x79']('\u5df2\u6ee1\u6700\u5c0f\u4eba\u6570\u3010' + _0x535802 + '\u3011\uff0c\u51fa\u53d1\u5012\u8ba1\u65f6' + _0x30aaae + '\u79d2', _0x3ac361), _0x30aaae = _0x30aaae - 0x5, leo['\x64\x65\x6c\x61\x79'](0x1388)['\x74\x68\x65\x6e'](() => _0x10132b(_0x4b886b, _0x535802));
                         else {
-                            leo['\x73\x61\x79']('\u5df2\u6ee1\u6700\u5c0f\u4eba\u6570\u3010' + _0x565641 + '\u3011\uff0c\u5012\u8ba1\u65f6\u7ed3\u675f\uff0c\u51fa\u53d1', _0x30128d), _0xcadc27 = [];
-                            var _0x346368 = 红叶の脚本['\x67\x65\x74\x54\x65\x61\x6d\x50\x6c\x61\x79\x65\x72\x73']();
-                            for (var _0x557b0e in _0x346368) {
-                                _0xcadc27[_0x557b0e] = _0x346368[_0x557b0e]['\x6e\x61\x6d\x65'];
+                            leo['\x73\x61\x79']('\u5df2\u6ee1\u6700\u5c0f\u4eba\u6570\u3010' + _0x535802 + '\u3011\uff0c\u5012\u8ba1\u65f6\u7ed3\u675f\uff0c\u51fa\u53d1', _0x3ac361), _0x286023 = [];
+                            var _0x709bda = 红叶の脚本['\x67\x65\x74\x54\x65\x61\x6d\x50\x6c\x61\x79\x65\x72\x73']();
+                            for (var _0x48c602 in _0x709bda) {
+                                _0x286023[_0x48c602] = _0x709bda[_0x48c602]['\x6e\x61\x6d\x65'];
                             }
-                            return leo['\x73\x61\x79']('\u6b22\u8fce\u4e58\u5750' + 班车旗号 + '\u306e\u73ed\u8f66\uff0c\u672c\u6b21\u65c5\u9014\u76ee\u7684\u5730\u662f\x5b\u627f\u8ba4\u4e4b\u6212\x5d\uff0c\u961f\u5458\x5b' + _0xcadc27['\x74\x6f\x53\x74\x72\x69\x6e\x67']() + '\x5d', _0x30128d), _0x37cb82 = 0x1e, leo['\x64\x6f\x6e\x65']();
+                            return leo['\x73\x61\x79']('\u6b22\u8fce\u4e58\u5750' + 班车旗号 + '\u306e\u73ed\u8f66\uff0c\u672c\u6b21\u65c5\u9014\u76ee\u7684\u5730\u662f\x5b\u627f\u8ba4\u4e4b\u6212\x5d\uff0c\u961f\u5458\x5b' + _0x286023['\x74\x6f\x53\x74\x72\x69\x6e\x67']() + '\x5d', _0x3ac361), _0x30aaae = 0x1e, leo['\x64\x6f\x6e\x65']();
                         }
                     } else
-                        return _0x37cb82 = 0x1e, leo['\x64\x65\x6c\x61\x79'](0x2710)['\x74\x68\x65\x6e'](() => _0x59bfc7(_0x52ca70, _0x565641));
+                        return _0x30aaae = 0x1e, leo['\x64\x65\x6c\x61\x79'](0x2710)['\x74\x68\x65\x6e'](() => _0x10132b(_0x4b886b, _0x535802));
                 }
             });
-        }, _0x3995fe = async () => {
-            await leo['\x77\x61\x69\x74\x41\x66\x74\x65\x72\x42\x61\x74\x74\x6c\x65'](), await leo['\x63\x68\x65\x63\x6b\x43\x72\x79\x73\x74\x61\x6c'](_0x342670), await leo['\x63\x68\x65\x63\x6b\x48\x65\x61\x6c\x74\x68'](_0x6e3dec);
-            !leo['\x68\x61\x73']('\u4fe1\u7b3a') && !leo['\x68\x61\x73']('\u56e2\u957f\u7684\u8bc1\u660e') && !leo['\x68\x61\x73']('\u602a\u7269\u788e\u7247') && !leo['\x68\x61\x73']('\u4fe1') && !leo['\x68\x61\x73']('\u627f\u8ba4\u4e4b\u6212') && (await leo['\x67\x6f\x74\x6f'](_0x267446 => _0x267446['\x63\x61\x73\x74\x6c\x65']['\x66\x31']), await leo['\x61\x75\x74\x6f\x57\x61\x6c\x6b\x4c\x69\x73\x74']([
+        }, _0xa1c25d = async () => {
+            await leo['\x77\x61\x69\x74\x41\x66\x74\x65\x72\x42\x61\x74\x74\x6c\x65'](), await leo['\x63\x68\x65\x63\x6b\x43\x72\x79\x73\x74\x61\x6c'](_0x319600), await leo['\x63\x68\x65\x63\x6b\x48\x65\x61\x6c\x74\x68'](_0x186d5d);
+            !leo['\x68\x61\x73']('\u4fe1\u7b3a') && !leo['\x68\x61\x73']('\u56e2\u957f\u7684\u8bc1\u660e') && !leo['\x68\x61\x73']('\u602a\u7269\u788e\u7247') && !leo['\x68\x61\x73']('\u4fe1') && !leo['\x68\x61\x73']('\u627f\u8ba4\u4e4b\u6212') && (await leo['\x67\x6f\x74\x6f'](_0x5c672f => _0x5c672f['\x63\x61\x73\x74\x6c\x65']['\x66\x31']), await leo['\x61\x75\x74\x6f\x57\x61\x6c\x6b\x4c\x69\x73\x74']([
                 [
                     0x4a,
                     0x13,
@@ -92,11 +92,11 @@ require(process.env.CGA_DIR_PATH_UTF8+'/leo').then(async (红叶の脚本) => {
                 ]
             ]), await leo['\x74\x61\x6c\x6b\x4e\x70\x63\x41\x74'](0x5, 0x3));
             if (leo['\x68\x61\x73']('\u4fe1\u7b3a')) {
-                await leo['\x67\x6f\x74\x6f'](_0x290da8 => _0x290da8['\x63\x61\x73\x74\x6c\x65']['\x78']), await leo['\x61\x75\x74\x6f\x57\x61\x6c\x6b'](_0x878356[候车点 - 0x1]), await leo['\x74\x75\x72\x6e\x44\x69\x72'](0x6), await leo['\x64\x65\x6c\x61\x79'](0x3e8);
-                var _0xdd3ea1 = 红叶の脚本['\x47\x65\x74\x50\x65\x74\x73\x49\x6e\x66\x6f']()['\x66\x69\x6e\x64'](_0xe2196c => _0xe2196c['\x72\x65\x61\x6c\x6e\x61\x6d\x65'] == 招牌宠名称);
-                _0xdd3ea1 && _0xdd3ea1['\x73\x74\x61\x74\x65'] != 0x3 && 红叶の脚本['\x43\x68\x61\x6e\x67\x65\x50\x65\x74\x53\x74\x61\x74\x65'](_0xdd3ea1['\x69\x6e\x64\x65\x78'], 红叶の脚本['\x50\x45\x54\x5f\x53\x54\x41\x54\x45\x5f\x52\x45\x53\x54']);
-                var _0x285e19 = 红叶の脚本['\x47\x65\x74\x50\x65\x74\x73\x49\x6e\x66\x6f']()['\x66\x69\x6e\x64'](_0x4ed162 => _0x4ed162['\x72\x65\x61\x6c\x6e\x61\x6d\x65'] == 出战宠名称);
-                _0x285e19 && _0x285e19['\x73\x74\x61\x74\x65'] != 0x2 && 红叶の脚本['\x43\x68\x61\x6e\x67\x65\x50\x65\x74\x53\x74\x61\x74\x65'](_0x285e19['\x69\x6e\x64\x65\x78'], 红叶の脚本['\x50\x45\x54\x5f\x53\x54\x41\x54\x45\x5f\x42\x41\x54\x54\x4c\x45']), await leo['\x64\x65\x6c\x61\x79'](0x3e8), await leo['\x74\x75\x72\x6e\x44\x69\x72'](0x2), await _0x59bfc7(队伍人数, 最小人数), await leo['\x61\x75\x74\x6f\x57\x61\x6c\x6b\x4c\x69\x73\x74']([
+                await leo['\x67\x6f\x74\x6f'](_0x43cb66 => _0x43cb66['\x63\x61\x73\x74\x6c\x65']['\x78']), await leo['\x61\x75\x74\x6f\x57\x61\x6c\x6b'](_0x4362d4[候车点 - 0x1]), await leo['\x74\x75\x72\x6e\x44\x69\x72'](0x6), await leo['\x64\x65\x6c\x61\x79'](0x3e8);
+                var _0x57fb74 = 红叶の脚本['\x47\x65\x74\x50\x65\x74\x73\x49\x6e\x66\x6f']()['\x66\x69\x6e\x64'](_0x16280b => _0x16280b['\x72\x65\x61\x6c\x6e\x61\x6d\x65'] == 招牌宠名称);
+                _0x57fb74 && _0x57fb74['\x73\x74\x61\x74\x65'] != 0x3 && 红叶の脚本['\x43\x68\x61\x6e\x67\x65\x50\x65\x74\x53\x74\x61\x74\x65'](_0x57fb74['\x69\x6e\x64\x65\x78'], 红叶の脚本['\x50\x45\x54\x5f\x53\x54\x41\x54\x45\x5f\x52\x45\x53\x54']);
+                var _0x43cfab = 红叶の脚本['\x47\x65\x74\x50\x65\x74\x73\x49\x6e\x66\x6f']()['\x66\x69\x6e\x64'](_0x1b06d0 => _0x1b06d0['\x72\x65\x61\x6c\x6e\x61\x6d\x65'] == 出战宠名称);
+                _0x43cfab && _0x43cfab['\x73\x74\x61\x74\x65'] != 0x2 && 红叶の脚本['\x43\x68\x61\x6e\x67\x65\x50\x65\x74\x53\x74\x61\x74\x65'](_0x43cfab['\x69\x6e\x64\x65\x78'], 红叶の脚本['\x50\x45\x54\x5f\x53\x54\x41\x54\x45\x5f\x42\x41\x54\x54\x4c\x45']), await leo['\x64\x65\x6c\x61\x79'](0x3e8), await leo['\x74\x75\x72\x6e\x44\x69\x72'](0x2), await _0x10132b(队伍人数, 最小人数), await leo['\x61\x75\x74\x6f\x57\x61\x6c\x6b\x4c\x69\x73\x74']([
                     [
                         0x29,
                         0x62,
@@ -171,7 +171,7 @@ require(process.env.CGA_DIR_PATH_UTF8+'/leo').then(async (红叶の脚本) => {
                             if (红叶の脚本['\x47\x65\x74\x4d\x61\x70\x4e\x61\x6d\x65']() == '\u9057\u8ff9')
                                 return !![];
                         }, ![]);
-                    } catch (_0x55dd3e) {
+                    } catch (_0x5834e5) {
                         await leo['\x6c\x6f\x67']('\u8ff7\u5bab\u5237\u65b0'), await leo['\x64\x65\x6c\x61\x79'](0x2710), await leo['\x61\x75\x74\x6f\x57\x61\x6c\x6b']([
                             0x2c,
                             0x16,
@@ -184,8 +184,8 @@ require(process.env.CGA_DIR_PATH_UTF8+'/leo').then(async (红叶の脚本) => {
                 红叶の脚本['\x47\x65\x74\x4d\x61\x70\x4e\x61\x6d\x65']() == '\u9057\u8ff9' && (await leo['\x61\x75\x74\x6f\x57\x61\x6c\x6b']([
                     0xf,
                     0xe
-                ]), _0x945fc2 ? await leo['\x74\x61\x6c\x6b\x4e\x70\x63'](0x4, leo['\x74\x61\x6c\x6b\x59\x65\x73']) : (await leo['\x74\x75\x72\x6e\x44\x69\x72'](0x2), await leo['\x6c\x6f\x67']('\u51c6\u5907\u597d\u540e\uff0c\u8f93\u5165\x33\u4e2a\x31\u7ee7\u7eed\uff0c\u8fdb\u5165\u6218\u6597'), await leo['\x77\x61\x69\x74\x4d\x65\x73\x73\x61\x67\x65\x55\x6e\x74\x69\x6c'](_0xd82e26 => {
-                    if (_0xd82e26['\x6d\x73\x67'] && _0xd82e26['\x6d\x73\x67']['\x69\x6e\x64\x65\x78\x4f\x66']('\x31\x31\x31') >= 0x0)
+                ]), _0x4b2c63 ? await leo['\x74\x61\x6c\x6b\x4e\x70\x63'](0x4, leo['\x74\x61\x6c\x6b\x59\x65\x73']) : (await leo['\x74\x75\x72\x6e\x44\x69\x72'](0x2), await leo['\x6c\x6f\x67']('\u51c6\u5907\u597d\u540e\uff0c\u8f93\u5165\x33\u4e2a\x31\u7ee7\u7eed\uff0c\u8fdb\u5165\u6218\u6597'), await leo['\x77\x61\x69\x74\x4d\x65\x73\x73\x61\x67\x65\x55\x6e\x74\x69\x6c'](_0x18d3a3 => {
+                    if (_0x18d3a3['\x6d\x73\x67'] && _0x18d3a3['\x6d\x73\x67']['\x69\x6e\x64\x65\x78\x4f\x66']('\x31\x31\x31') >= 0x0)
                         return !![];
                 }), await leo['\x74\x61\x6c\x6b\x4e\x70\x63'](0x4, leo['\x74\x61\x6c\x6b\x59\x65\x73'])), await leo['\x77\x61\x69\x74\x41\x66\x74\x65\x72\x42\x61\x74\x74\x6c\x65']()), 红叶の脚本['\x47\x65\x74\x4d\x61\x70\x4e\x61\x6d\x65']() == '\u7814\u7a76\u5ba4' && (await leo['\x6c\x65\x61\x76\x65\x54\x65\x61\x6d'](), await leo['\x74\x61\x6c\x6b\x4e\x70\x63\x41\x74'](0xe, 0xe));
             }
@@ -193,9 +193,9 @@ require(process.env.CGA_DIR_PATH_UTF8+'/leo').then(async (红叶の脚本) => {
         };
     return await leo['\x6c\x6f\x6f\x70'](async () => {
         try {
-            await _0x3995fe();
-        } catch (_0x1c7d16) {
-            console['\x6c\x6f\x67'](leo['\x6c\x6f\x67\x54\x69\x6d\x65']() + '\u8fd0\u884c\u51fa\u9519\uff0c\x65\x3a' + _0x1c7d16), await leo['\x64\x65\x6c\x61\x79'](0x3e8 * 0x1e);
+            await _0xa1c25d();
+        } catch (_0x261f44) {
+            console['\x6c\x6f\x67'](leo['\x6c\x6f\x67\x54\x69\x6d\x65']() + '\u8fd0\u884c\u51fa\u9519\uff0c\x65\x3a' + _0x261f44), await leo['\x64\x65\x6c\x61\x79'](0x3e8 * 0x1e);
         }
         await leo['\x64\x65\x6c\x61\x79'](0x3e8);
     }), leo['\x65\x78\x69\x74']();
